5d8e5ab7fd27f5a49e99c809cf01b87cd0b3ec11
Venu Gudavalli <venugudavalli@yahoo.com>
2018-12-07 19:01:56 +0000

    Added permissions and authentication to our user ProfileViewSet

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -3,9 +3,12 @@ from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
 
 from . import serializers
 from . import models
+from . import permissions
+
 
 # Create your views here.
 class HelloAPIView(APIView):
@@ -90,6 +93,9 @@ class HelloViewSet(viewsets.ViewSet):
         return Response({'http_method': 'DELETE'})
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    """Handles creating, creating and updating profiles"""
+    """Handles creating, creating and updating profiles."""
+
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
